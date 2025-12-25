4498720d6d079a776428e542acf5663ae2d14161
Eng Aisha <163716646+AishaaAbdi@users.noreply.github.com>
2025-12-25 17:32:10 +0300

    Update allHouse.dart

--- lib/allHouse.dart
+++ lib/allHouse.dart
@@ -1,94 +1,125 @@
 import 'package:flutter/material.dart';
 import 'package:foodorder/Homedetail.dart';
 
-
-class allHouse extends StatefulWidget {
-  final String img;
-  final  String text;
+class Allhouse extends StatelessWidget {
+   final  String img;
+   final  String text;
+    final   double price;
   final   String subtext;
-   final   double price;
-  const allHouse({
-    required this.img,
+  final String bedrooms;
+  final String bathrooms;
+  final String square;
+  final String description;
+  
+  const Allhouse({
+     required this.img,
      required this.text,
+      required this.price,
       required this.subtext,
-       required this.price,
+      required this.bedrooms,
+    required this.bathrooms,
+    required this.square,
+    required this.description,
+      
   });
 
   @override
-  State<allHouse> createState() => _AllfoodState();
-}
-
-class _AllfoodState extends State<allHouse> {
- 
-
-  
-  @override
   Widget build(BuildContext context) {
-    return
-      
-            Container(
-              height: 310,
-              width: 280,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                color: Colors.white
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(15),
-                        topRight: Radius.circular(15)
-                      ),
-                      child: Image.network(widget.img,
-                      height: 150,
-                      width: double.infinity,
-                      fit: BoxFit.cover,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(5.0),
-                      child: Text(widget.text,
-                      style: TextStyle(fontSize: 22,
-                      fontWeight: FontWeight.bold),),
-                    ),
-                     Padding(
-                      padding: const EdgeInsets.all(5.0),
-                      child: Text(widget.subtext,
-                      style: TextStyle(fontSize: 22,
-                    ),),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
 
-                          Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Text("\$${widget.price}",
-                      style: TextStyle(fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.blue.shade900),),
-                    ),
-                    ElevatedButton(onPressed: (){
-                      Navigator.push(context,
-                       MaterialPageRoute(
-                        builder:(context)=>Homedetail()));
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.green,
-                      foregroundColor: Colors.white
-                    ),
-                     child: Text("View Details",
+
+  return Container(
+    // height: 150,
+        width: 180,
+            decoration: BoxDecoration(
+              color: Colors.blue.shade900,
+              borderRadius: BorderRadius.circular(20),),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(15),
+            child: Image.network(
+              '$img',
+              height: 140,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+          ),
+        )
+,
+        
+                  Row(
+                    // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          text,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white
+                          ),
+                        ),
                       ),
-                      )
-                      ],
-                    )
                     
-                ],
-              ),
-            );
+                       Text(
+                          "\$$price",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      
+                    ],
+                  ),
 
-    
-  }
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      subtext,
+                      style: TextStyle(fontSize: 18,
+                      color: Colors.white),
+                    ),
+                  ),
+
+                  SizedBox(height: 16),
+                  SizedBox(
+                    height: 45,
+                    child: ElevatedButton(onPressed: (){
+                                       Navigator.push(context,
+                       MaterialPageRoute(
+                        builder:(context)=>Homedetail(
+                         homename: text,
+                      city: subtext,
+                      price: price,
+                      img: img,
+                      Bedrooms: bedrooms,
+                      bathroom: bathrooms,
+                      square: square,
+                      description: description,
+                           )
+                           ));      
+                                            },
+                                            style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        foregroundColor: Colors.blue.shade900,
+                        fixedSize: Size(350, 50)
+                                            ),
+                                             child: Text("View Detail",
+                                              style: TextStyle(fontSize: 18,
+                     ),
+                    ),
+                        ),
+                        ),
+                  
+      ],
+    ),
+  
+  );
 }
+
+  }

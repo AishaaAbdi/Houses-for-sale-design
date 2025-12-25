7081e2b5791bda34ea7ef1f04fd152cafaffa2e4
Eng Aisha <163716646+AishaaAbdi@users.noreply.github.com>
2025-12-25 19:25:28 +0300

    Update Homedetail.dart

--- lib/Homedetail.dart
+++ lib/Homedetail.dart
@@ -3,23 +3,23 @@ import 'package:foodorder/contact.dart';
 
 class Homedetail extends StatelessWidget {
   final String homename;
-    final String city;
+  final String city;
   final String img;
 
   final double price;
   final String Bedrooms;
   final String bathroom;
   final String square;
-   final String description;
+  final String description;
   const Homedetail({
     required this.homename,
-       required this.city,
-       required this.price,
-          required this.img,
-          required this.Bedrooms,
-             required this.bathroom,
-                required this.square,
-                 required this.description,
+    required this.city,
+    required this.price,
+    required this.img,
+    required this.Bedrooms,
+    required this.bathroom,
+    required this.square,
+    required this.description,
   });
 
   @override
@@ -29,131 +29,123 @@ class Homedetail extends StatelessWidget {
         foregroundColor: Colors.white,
         backgroundColor: Colors.blue.shade900,
         // centerTitle: true,
-        leading: IconButton(onPressed: (){
-          Navigator.pop(context);
-        },
-         icon: Icon(Icons.arrow_back_ios,size: 30,)),
-         title: Column(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back_ios, size: 30),
+        ),
+        title: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-           children: [
-             Text("$homename",style: TextStyle(
-              fontSize: 25,fontWeight: FontWeight.bold
-             ),),
-             Row(
+          children: [
+            Text(
+              "$homename",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            Row(
               // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text("$city",),
-                SizedBox(width: 12,),
-                Text("\$$price",)
-              ],
-             )
-           ],
-         ),
+              children: [Text("$city"), SizedBox(width: 12), Text("\$$price")],
+            ),
+          ],
+        ),
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-         
-         
-            ClipRRect(
-              
-                child: Image.network("$img",
-                fit: BoxFit.cover,
-                height: 218,
-                width: double.infinity,
-                ),
-              ),
-          
+          ClipRRect(
+            child: Image.network(
+              "$img",
+              fit: BoxFit.cover,
+              height: 218,
+              width: double.infinity,
+            ),
+          ),
+
           Padding(
             padding: const EdgeInsets.all(10.0),
             child: Row(
               // mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text("\$$price",
-                        style: TextStyle(fontSize: 25,
-                        fontWeight: FontWeight.bold)),
-                        SizedBox(width: 15,),
-                        Text("For Sale",
-                        style: TextStyle(fontSize: 25,
-                      
-                        ))
+                Text(
+                  "\$$price",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(width: 15),
+                Text("For Sale", style: TextStyle(fontSize: 25)),
               ],
             ),
+          ),
+          Divider(indent: 13),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Text(
+              "key Details",
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Icon(Icons.bed, size: 30),
+                SizedBox(width: 10),
+                Text("$Bedrooms", style: TextStyle(fontSize: 22)),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Icon(Icons.bathroom, size: 30),
+                SizedBox(width: 10),
+                Text("$bathroom", style: TextStyle(fontSize: 22)),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Icon(Icons.square_foot, size: 30),
+                SizedBox(width: 10),
+                Text("$square", style: TextStyle(fontSize: 22)),
+              ],
+            ),
+          ),
+          Divider(indent: 13),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Text("$description", style: TextStyle(fontSize: 24)),
+          ),
 
-          ),
-          Divider(
-            indent: 13,
-          ),
-           Padding(
-             padding: const EdgeInsets.all(12.0),
-             child: Text("key Details",
-                        style: TextStyle(fontSize: 22,
-                        fontWeight: FontWeight.bold)),
-           ),
-           Padding(
-             padding: const EdgeInsets.all(8.0),
-             child: Row(
-              children: [
-                Icon(Icons.bed,size: 30,),
-                SizedBox(width: 10,),
-                 Text("$Bedrooms",
-                        style: TextStyle(fontSize: 22,
-                        )),
-              ],
-             ),
-           ),
-            Padding(
-             padding: const EdgeInsets.all(8.0),
-             child: Row(
-              children: [
-                Icon(Icons.bathroom,size: 30,),
-                SizedBox(width: 10,),
-                 Text("$bathroom",
-                        style: TextStyle(fontSize: 22,
-                        )),
-              ],
-             ),
-            ),
-             Padding(
-             padding: const EdgeInsets.all(8.0),
-             child: Row(
-              children: [
-                Icon(Icons.square_foot,size: 30,),
-                SizedBox(width: 10,),
-                 Text("$square",
-                        style: TextStyle(fontSize: 22,
-                        )),
-              ],
-             ),
-             ),
-           Divider(
-            indent: 13,
-          ),
           Padding(
             padding: const EdgeInsets.all(10.0),
-            child: Text("$description",
-                        style: TextStyle(fontSize: 24,
-                        )),
-          ),
-          
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: ElevatedButton(onPressed: (){
-               Navigator.push(context,
-                       MaterialPageRoute(
-                        builder:(context)=>Contact()));
-                    
-            },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.blue.shade900,
-              foregroundColor: Colors.white,
-              fixedSize: Size(340, 50)
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder:
+                        (context) => Contact(
+                          homename: homename,
+                          city: city,
+                          price: price,
+                          img: img,
+                        ),
+                  ),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue.shade900,
+                foregroundColor: Colors.white,
+                fixedSize: Size(340, 50),
+              ),
+              child: Text("Contact Owner"),
             ),
-             child:Text("Contact Owner")),
-          )
+          ),
         ],
       ),
-     
     );
   }
 }
